f39a0c5e35f4e5205badd6eb4ac16e1d0970758d
saipraveenkumar <sai_katteboina@simplicity-creations.com>
2017-09-07 18:51:42 +0530

    Initial Commit.

---
+++ LocalizationDemo/DemoViewController.swift
@@ -0,0 +1,51 @@
+//
+//  DemoViewController.swift
+//  LocalizationDemo
+//
+//  Created by Rushikesh Kulkarni on 07/09/17.
+//  Copyright © 2017 simplicity. All rights reserved.
+//
+
+import UIKit
+
+class DemoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+}
+extension UIView {
+    
+    func anchorToTop(_ top: NSLayoutYAxisAnchor? = nil, left: NSLayoutXAxisAnchor? = nil, bottom: NSLayoutYAxisAnchor? = nil, right: NSLayoutXAxisAnchor? = nil) {
+        
+        anchorWithConstantsToTop(top, left: left, bottom: bottom, right: right, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0)
+    }
+    
+    func anchorWithConstantsToTop(_ top: NSLayoutYAxisAnchor? = nil, left: NSLayoutXAxisAnchor? = nil, bottom: NSLayoutYAxisAnchor? = nil, right: NSLayoutXAxisAnchor? = nil, topConstant: CGFloat = 0, leftConstant: CGFloat = 0, bottomConstant: CGFloat = 0, rightConstant: CGFloat = 0) {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            topAnchor.constraint(equalTo: top, constant: topConstant).isActive = true
+        }
+        
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: -bottomConstant).isActive = true
+        }
+        
+        if let left = left {
+            leftAnchor.constraint(equalTo: left, constant: leftConstant).isActive = true
+        }
+        
+        if let right = right {
+            rightAnchor.constraint(equalTo: right, constant: -rightConstant).isActive = true
+        }
+        
+    }
+    
+}
--- LocalizationDemo/ViewController.swift
+++ LocalizationDemo/ViewController.swift
@@ -10,16 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstNameLbl: UILabel!
+    @IBOutlet weak var lastNameLbl: UILabel!
+    @IBOutlet weak var secondViewBtn: UIButton!
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.firstNameLbl.text = NSLocalizedString("first_name", comment: "first name")
+        self.lastNameLbl.text = NSLocalizedString("last_name", comment: "last name")
+        self.secondViewBtn.setTitle(NSLocalizedString("second_view", comment: "second"), for: .normal)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func changeToArbic(_ sender: Any) {
+        var transition: UIViewAnimationOptions = .transitionFlipFromLeft
+        if L102Language.currentAppleLanguage() == "en" {
+            L102Language.setAppleLAnguageTo(lang: "ar")
+            UIView.appearance().semanticContentAttribute = .forceRightToLeft
+        } else {
+            L102Language.setAppleLAnguageTo(lang: "en")
+            transition = .transitionFlipFromRight
+            UIView.appearance().semanticContentAttribute = .forceLeftToRight
+        }
+        let rootviewcontroller: UIWindow = ((UIApplication.shared.delegate?.window)!)!
+        rootviewcontroller.rootViewController = self.storyboard?.instantiateViewController(withIdentifier: "ViewController")
+        let mainwindow = (UIApplication.shared.delegate?.window!)!
+        mainwindow.backgroundColor = UIColor(hue: 0.6477, saturation: 0.6314, brightness: 0.6077, alpha: 0.8)
+        UIView.transition(with: mainwindow, duration: 0.55001, options: transition, animations: { () -> Void in
+        }) { (finished) -> Void in
+            
+        }
     }
-
+    
+    @IBAction func secondBtnAction(_ sender: UIButton) {
+        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+        let seocndVC = storyBoard.instantiateViewController(withIdentifier: "SecondViewController") as! SecondViewController
+        self.present(seocndVC, animated: true, completion: nil)
+    }
 
 }
 
